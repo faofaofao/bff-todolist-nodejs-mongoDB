5d609613ee0eb58b3bc714c9be7dd91c8aae4867
Pedro Lavin <pacuevasl@gmail.com>
2025-02-25 14:48:49 -0300

    feat: Server connect config

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res)=> {
+    res.send('Hello World');
+    console.log('Hello World');
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
